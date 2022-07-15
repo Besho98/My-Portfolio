5b01ae882b3bb4e78897f5792b2e1d71e3c2b556
Beshoy Hamam <88683405+Besho98@users.noreply.github.com>
2022-07-15 23:15:49 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Toggle Menu
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function () {
+    
+    tLinks.classList.toggle("open")
+};
